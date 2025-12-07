34d16b5bad7487e3933b50d60bff3bf86f00d61f
Yashwanth VG <vgyashwanthkumar@gmail.com>
2025-12-08 00:18:33 +0530

    Initial Commit

--- Transmitter_Code/Transmitter_Code.ino
+++ Transmitter_Code/Transmitter_Code.ino
@@ -8,15 +8,14 @@
 #define CE_PIN 2
 #define CSN_PIN 4
 
-#define DIGITAL_CH1 6
-#define DIGITAL_CH2 7
-#define DIGITAL_CH3 9
-#define DIGITAL_CH4 8
-#define THROTTLE A6
-#define STEERING A7
-#define HEAD_POT A5
-#define TOP_TRIM_POT A4
-#define DOWN_TRIM_POT A3
+#define TANK1 0
+#define TANK2 1
+
+#define TANK1_SENSOR_TRIG 3
+#define TANK1_SENSOR_ECHO 5
+#define TANK2_SENSOR_TRIG 6
+#define TANK2_SENSOR_ECHO 7
+
 
 RF24 radio(CE_PIN, CSN_PIN); // CE, CS
 // address of the nodes
@@ -26,48 +25,38 @@ const byte address[6] = "00001";
 //Data packet
 struct DataPacket
 {
-  uint16_t throttle;
-  uint8_t streeing;
-  uint8_t head_pot;
-  uint8_t down_trim_pot;
-  uint8_t top_trim_pot;
-  uint8_t digital_ch1 : 1;
-  uint8_t digital_ch2 : 1;
-  uint8_t digital_ch3 : 1;
-  uint8_t digital_ch4 : 1;
-  uint8_t left_turn   : 1;
-  uint8_t right_turn  : 1;
-  uint8_t forward     : 1;
-  uint8_t backward    : 1;
+  uint16_t tank1_depth;
+  uint16_t tank2_depth;
 };
 
-void ProcessData(DataPacket* Data);
+void ProcessData(struct DataPacket * Data);
+uint16_t MeasureDepthInCm(bool tank);
 
 void setup() 
 {
   Serial.begin(9600);
 
-  // !!!Warning!!! dont remove this 
-  // lead to Hardware Error
-  analogReference(EXTERNAL); // use AREF for reference voltage
+  // // !!!Warning!!! dont remove this 
+  // // lead to Hardware Error
+  // analogReference(EXTERNAL); // use AREF for reference voltage
 
   // NRF Settings
   radio.begin();
-  // (2400MHz + 100MHz) Frequency Channel
+  // (2400MHz + 50MHz) Frequency Channel
   // both Transimitter and Receiver should be on same channel
   // helps when multiple NRF modules communicating in same space
-  radio.setChannel(100); 
+  radio.setChannel(50); 
   radio.setDataRate(RF24_250KBPS);
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.setPayloadSize(sizeof(DataPacket));
   radio.stopListening();
 
-  // Hardware Settings
-  pinMode(DIGITAL_CH1,INPUT_PULLUP);
-  pinMode(DIGITAL_CH2,INPUT_PULLUP);
-  pinMode(DIGITAL_CH3,INPUT_PULLUP);
-  pinMode(DIGITAL_CH4,INPUT_PULLUP);
+  // // Hardware Settings
+  pinMode(TANK1_SENSOR_TRIG, OUTPUT);
+  pinMode(TANK1_SENSOR_ECHO, INPUT);
+  pinMode(TANK2_SENSOR_TRIG,OUTPUT);
+  pinMode(TANK2_SENSOR_ECHO,INPUT);
 
   
 
@@ -75,8 +64,10 @@ void setup()
 
 void loop() 
 {   
-  DataPacket data;
+  struct DataPacket data;
   ProcessData(&data);
+
+  // send data
   radio.write(&data, sizeof(data));
   
 }
@@ -84,79 +75,47 @@ void loop()
 void ProcessData(DataPacket* data)
 {
 
-    uint16_t AnalogValA6 = analogRead(THROTTLE);
-    uint16_t AnalogValA7 = analogRead(STEERING);
-    uint16_t AnalogValA5 = analogRead(HEAD_POT);
-    bool DigitalValCH1 = digitalRead(DIGITAL_CH1);
-    bool DigitalValCH3 = digitalRead(DIGITAL_CH3);
+    data->tank1_depth = MeasureDepthInCm(TANK1);
+    data->tank2_depth = MeasureDepthInCm(TANK2);
 
+}
+
+uint16_t MeasureDepthInCm(bool tank)
+{
+
+  uint32_t duration;
+  uint16_t distance;
+  if(tank == TANK1)
+  {
+    digitalWrite(TANK1_SENSOR_TRIG, LOW);
+    delayMicroseconds(5);
+
+    digitalWrite(TANK1_SENSOR_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TANK1_SENSOR_TRIG, LOW);
+
+    duration = pulseIn(TANK1_SENSOR_ECHO, HIGH);
     
-    
-    // Throttle
-    if(AnalogValA6 < 287)
-    {
-      data->forward = true;
-      data->backward = false;
-      data->throttle = (uint16_t)(map(AnalogValA6,155, 287, 2000, 1500));
-    }
-    else if(AnalogValA6 > 295)
-    {
-      data->forward = false;
-      data->backward = true;
-      data->throttle = (uint16_t)(map(AnalogValA6,295, 382, 1500, 1000));
-    }
-    else 
-    {
-      data->forward = false;
-      data->backward = false;
-      data->throttle = 1500; // stop the motor
-    }
+  }  
+  else
+  {
+    digitalWrite(TANK2_SENSOR_TRIG, LOW);
+    delayMicroseconds(5);
 
-    // Steering 
-    if(AnalogValA7 < 550)
-    {
-      data->right_turn = true;
-      data->left_turn = false;
-      uint8_t angle = (uint8_t)(map(AnalogValA7,550, 325, (90+data->head_pot), 90+45));
-      data->streeing = angle;
-    }
-    else if(AnalogValA7 > 565)
-    {
-      data->right_turn = false;
-      data->left_turn = true;
-      uint8_t angle = (uint8_t)(map(AnalogValA7,565, 745, (90+data->head_pot), 90-45));
-      data->streeing = angle;
-    }
-    else 
-    {
-      data->right_turn = false;
-      data->left_turn = false;
+    digitalWrite(TANK2_SENSOR_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TANK2_SENSOR_TRIG, LOW);
 
-       // Steering Trim
-      if(((int16_t)(600-(int16_t)AnalogValA5)>0) && (AnalogValA5 > 200))
-      {
-        data->head_pot = (uint8_t)((600-AnalogValA5)/10);
-        data->streeing = (90+data->head_pot);
-      }
-      else if(((int16_t)((int16_t)AnalogValA5-600)>0) && (AnalogValA5 < 1000))
-      {
-        data->head_pot = (uint8_t)((AnalogValA5-600)/10);
-        data->streeing = (90-data->head_pot);
-      }
-      else
-      {
-        data->head_pot = (uint8_t)0; 
-        data->streeing = (90+data->head_pot);
-      }
-      
-    }
+    duration = pulseIn(TANK2_SENSOR_ECHO, HIGH);
 
-    data->digital_ch1 = DigitalValCH1;
-    data->digital_ch3 = DigitalValCH3;
+  }
 
-    
-    
-  
+  // Calculate the Depth in cm
+  distance = (duration * 0.034 / 2);
+
+  return distance;
+
+
 }
 
 
